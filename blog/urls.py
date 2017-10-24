985c3cd9c8d5677b97c160dc3f80ccfb910022eb
Ravhi Rizaldi <ravhi.rizaldi@outlook.com>
2017-10-24 15:45:42 +0700

    added view detailed and css for the site

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,19 @@ urlpatterns = [
 	# This regular expression will match ^ (a beginning) followed by $ (an end)
 	# so only an empty string will match.
 
-	# That's correct, because in Django URL resolvers, 
+	# That's correct, because in Django URL resolvers,
 	# 'http://127.0.0.1:8000/' is not a part of the URL.
 
-	# This pattern will tell Django that views.post_list is the right place to go 
+	# This pattern will tell Django that views.post_list is the right place to go
 	# if someone enters your website at the 'http://127.0.0.1:8000/' address.
 	url(r'^$', views.post_list, name="post_list"),
+
+	# it starts with ^ again – "the beginning".
+	# post/ just means that after the beginning, the URL should contain the word post and a /.
+	# (?P<pk>\d+) – this part is trickier. It means that Django will take everything that you place here
+	# and transfer it to a view as a variable called pk.
+
+	# (Note that this matches the name we gave the primary key variable back in blog/templates/blog/post_list.html!)
+	# \d also tells us that it can only be a digit, not a letter (so everything between 0 and 9).
+	url(r'article/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 	]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
 # Create your views here.
 
-# function (def) called post_list that takes request and return a 
+# function (def) called post_list that takes request and return a
 # function render that will render (put together) our template blog/post_list.html
 def post_list(request):
 	# sort and filter post with published_date
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	# {} is for template name using it for template in HTML
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
